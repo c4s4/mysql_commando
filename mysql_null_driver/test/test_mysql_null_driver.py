981006e6e0bd1b431f67eaf6739a3b7e88c12e6d
Michel Casabianca <casa@sweetohm.net>
2014-03-25 14:15:19 +0100

    Fixed tests

--- mysql_null_driver/test/test_mysql_null_driver.py
+++ mysql_null_driver/test/test_mysql_null_driver.py
@@ -3,7 +3,7 @@
 
 import os
 import sys
-sys.path.append(os.path.join(os.path.dirname(__file__), '..', 'src'))
+sys.path.append(os.path.join(os.path.dirname(__file__), '..', '..'))
 import unittest
 import datetime
 import mysql_null_driver
